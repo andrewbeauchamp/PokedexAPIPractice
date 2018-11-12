9bc95331e308e394238bfef7bf50285d362d3daf
Andrew Beauchamp <andrewbeauchamp@macbook-pro-55.local>
2018-11-12 12:08:40 -0600

    Added first responder to the pokemonIDTextField and put a guard let in view did load to make sure the text field had a value

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    let pokemonAPIBaseURL = "https://pokeapi.co/docs/v2"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        pokemonIDTextField.resignFirstResponder()
+        
+        //Checking to make sure the text field has a value
+        guard let pokemonID = pokemonIDTextField.text else {
+            return
+        }
+        
     }
 
-
+    @IBOutlet weak var pokemonIDTextField: UITextField!
+    @IBOutlet weak var pokemonDescTextView: UITextView!
+    
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
 }
 
